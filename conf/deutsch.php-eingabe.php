fa9c8d3fd4ae5efd3d6b7b8a9f25961224942802
Andreas <Andreas@Voelkl.online>
2021-03-30 21:26:33 +0200

    Kein uppercase

--- conf/deutsch.php-eingabe.php
+++ conf/deutsch.php-eingabe.php
@@ -6,18 +6,18 @@ $t['farbe1'] = "Es wurde eine neue Farbe gesetzt, %u_nick%";
 $t['farbe2'] = "Farbe:";
 $t['eingabe1'] = "Zeilen";
 
-$t['menue1'] = "RÄUME";
-$t['menue2'] = "USER";
-$t['menue3'] = "EINSTELLUNGEN";
-$t['menue4'] = "HILFE";
-$t['menue5'] = "SPERREN";
-$t['menue6'] = "ENDE";
-$t['menue7'] = "LOG";
-$t['menue8'] = "ANTWORTEN";
-$t['menue9'] = "RESET";
-$t['menue10'] = "MAIL";
-$t['menue11'] = "FORUM";
-$t['menue12'] = "BLACKLIST";
-$t['menue13'] = "UMFRAGE";
+$t['menue1'] = "Räume";
+$t['menue2'] = "User";
+$t['menue3'] = "Einstellungen";
+$t['menue4'] = "Hilfe";
+$t['menue5'] = "Sperren";
+$t['menue6'] = "Logout";
+$t['menue7'] = "Log";
+$t['menue8'] = "Antworten";
+$t['menue9'] = "Reset";
+$t['menue10'] = "Mail";
+$t['menue11'] = "Forum";
+$t['menue12'] = "Blacklist";
+$t['menue13'] = "Umfrage";
 
 ?>
