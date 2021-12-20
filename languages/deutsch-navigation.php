2ffbf309f4698eadb9fedee3f4336dfdef47f445
Andreas <Andreas@Voelkl.online>
2021-12-20 11:29:35 +0100

    Anpassung der Übersetzungen

--- languages/deutsch-navigation.php
+++ languages/deutsch-navigation.php
@@ -1,23 +1,20 @@
 <?php
-
 // Übersetzungen von "Navigation"
 
-$t['navigation_menue1'] = "$chat";
-$t['navigation_menue2'] = "Chat";
-$t['navigation_menue3'] = "Räume";
-$t['navigation_menue4'] = "Benutzer";
-$t['navigation_menue5'] = "Nachrichten";
-$t['navigation_menue6'] = "Forum";
-$t['navigation_menue7'] = "Einstellungen";
-$t['navigation_menue8'] = "Sperren";
-$t['navigation_menue9'] = "Profil";
-$t['navigation_menue10'] = "Statistik";
-$t['navigation_menue11'] = "Antworten";
-$t['navigation_menue12'] = "Log";
-$t['navigation_menue13'] = "Reset";
-$t['navigation_menue14'] = "Hilfe";
-$t['navigation_menue15'] = "Logout";
-$t['navigation_menue16'] = "Freunde";
+$t['navigation_chat'] = "Chat";
+$t['navigation_raeume'] = "Räume";
+$t['navigation_benutzer'] = "Benutzer";
+$t['navigation_nachrichten'] = "Nachrichten";
+$t['navigation_forum'] = "Forum";
+$t['navigation_einstellungen'] = "Einstellungen";
+$t['navigation_sperren'] = "Sperren";
+$t['navigation_profil'] = "Profil";
+$t['navigation_freunde'] = "Freunde";
+$t['navigation_statistik'] = "Statistik";
+$t['navigation_definierte_antworten'] = "Definierte Antworten";
+$t['navigation_log'] = "Log";
+$t['navigation_hilfe'] = "Hilfe";
+$t['navigation_logout'] = "Logout";
 
 $t['zum_chat'] = "Zum Chat";
 $t['forum_interaktiv_einzahl'] = "Insgesamt ist 1 Benutzer online.";
