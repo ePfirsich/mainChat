4192db2b7717f9e3b1b21bb2502109c97c78c23e
Andreas <Andreas@Voelkl.online>
2022-02-04 15:40:57 +0100

    Aktualisierung auf mainChat 7.1.2

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 // Version / Copyright - nicht entfernen!
-$mainchat_version = "mainChat 7.1.1 © by <a href=\"https://www.fidion.de\" target=\"_blank\">fidion GmbH</a> 1999-2018 - Ab Version 7.0 durch <a href=\"https://www.anime-community.de\" target=\"_blank\">Andreas Völkl</a> auf <a href=\"https://github.com/ePfirsich/mainChat\" target=\"_blank\">GitHub</a>";
+$mainchat_version = "mainChat 7.1.2 © by <a href=\"https://www.fidion.de\" target=\"_blank\">fidion GmbH</a> 1999-2018 - Ab Version 7.0 durch <a href=\"https://www.anime-community.de\" target=\"_blank\">Andreas Völkl</a> auf <a href=\"https://github.com/ePfirsich/mainChat\" target=\"_blank\">GitHub</a>";
 
 // Caching unterdrücken
 Header("Last-Modified: " . gmDate("D, d M Y H:i:s", Time()) . " GMT");
