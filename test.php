9e0c0cc2d7a95327df2d7997c2e762aeaaf329fd
Andreas <Andreas@Voelkl.online>
2022-07-31 00:32:47 +0200

    Test aktualisieren

--- test.php
+++ test.php
@@ -6,10 +6,10 @@ require_once("functions/functions-chat_lese.php");
 $trigger_letzte_Zeilen = 1;
 
 // Benutzerdaten setzen
-id_lese($id);
+id_lese();
 
 // Direkten Aufruf der Datei verbieten (nicht eingeloggt)
-if( !isset($u_id) || $u_id == NULL || $u_id == "") {
+if( false && !isset($u_id) || $u_id == NULL || $u_id == "") {
 	echo "<body onLoad='parent.location.href=\"index.php\"'>\n";
 	header('Location: ' . $chat_url);
 	exit();
@@ -40,12 +40,11 @@ if( !isset($u_id) || $u_id == NULL || $u_id == "") {
 	</script>';
 	
 	$meta_refresh .= "<meta http-equiv=\"expires\" content=\"0\">\n";
-	$meta_refresh .= "<script>\n 
-						window.setInterval(function() {
-							var elem = document.getElementById('container');
-							elem.scrollTop = elem.scrollHeight;
-						}, 5000);
-					</script>\n";
+	$meta_refresh .= "<script>
+	setInterval(() => {
+		document.getElementById('content-wrapper').scrollTo(1,300000);
+	}, 100);
+	</script>\n";
 	//$meta_refresh .= '<script src="js/jscript.js"></script>';
 	$meta_refresh .= '<script src="js/jquery-3.6.0.min.js"></script>';
 	$meta_refresh .= '<script src="js/chat.js"></script>';
@@ -98,7 +97,10 @@ if( !isset($u_id) || $u_id == NULL || $u_id == "") {
 			Top nav
 		</div>
 		<div id="content-wrapper">
-			<div id="view_ajax"></div>
+		<?php $i = 0;?>
+			text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>
+			text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>text<?php echo $i; $i++;?><br>
+			<!-- <div id="view_ajax"></div> -->
 		</div>
 		<div id="seitenleiste"></div>
 	</div>
