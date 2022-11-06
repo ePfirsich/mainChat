bd240d56bc36b8a67a1a12bf2d724986f930ee94
Andreas <Andreas@Voelkl.online>
2022-11-06 23:27:56 +0100

    Fix: Texte von ignorierten Benutzern im Chat ausblenden

--- class.chat.php
+++ class.chat.php
@@ -9,11 +9,12 @@ $u_id = $_SESSION['u_id'];
 
 class chatClass {
 	public static function getRestChatLines($last_time_id) {
-		global $chat_status_klein, $u_nick;
+		global $chat_status_klein, $u_nick, $ignore;
+		
 		$arr = array();
 		$jsonData = '{"results":[';
 		$refresh_zeit = 0;
-	//	$query = "";
+		// $query = "";
 		$o_raum = "";
 		
 		
@@ -69,104 +70,110 @@ class chatClass {
 		
 		$result = $query->fetchAll();
 		foreach($result as $zaehler => $row) {
-			$query_usr = pdoQuery("SELECT `u_id`,`u_farbe`, `u_level` FROM `user` WHERE `u_id` = :u_id", [':u_id'=>$u_id]);
+			// Die Ignorierten Benutzer rausfiltern
+			$ausgeben = true;
+			if (isset($ignore[$row['c_von_user_id']]) && $ignore[$row['c_von_user_id']]) {
+				$ausgeben = false;
+			}
 			
-			$row_usr = $query_usr->fetch();
-			$usr_farbe = $row_usr['u_farbe'];
-			$times = date('H:i:s', strtotime($row['c_zeit']));
-			$ava = "";
-			$level = "";
-			
-			if( $row['c_typ'] == 'S' ) {
-				$vonuserid = "";
-			} else {
-				if($benutzerdaten['u_avatare_anzeigen'] == 1) {
-					$query2 = pdoQuery("SELECT `ui_geschlecht` FROM `userinfo` WHERE `ui_userid` = :ui_userid", [':ui_userid'=>$row['c_von_user_id']]);
-					$result2Count = $query2->rowCount();
-					if ($result2Count == 1) {
-						$result2 = $query2->fetch();
-						$ui_gen = $result2['ui_geschlecht'];
-					} else {
-						$ui_gen = '0';
+			if ($ausgeben) {
+				$query_usr = pdoQuery("SELECT `u_id`,`u_farbe`, `u_level` FROM `user` WHERE `u_id` = :u_id", [':u_id'=>$u_id]);
+				
+				$row_usr = $query_usr->fetch();
+				$usr_farbe = $row_usr['u_farbe'];
+				$times = date('H:i:s', strtotime($row['c_zeit']));
+				$ava = "";
+				$level = "";
+				
+				if( $row['c_typ'] == 'S' ) {
+					$vonuserid = "";
+				} else {
+					if($benutzerdaten['u_avatare_anzeigen'] == 1) {
+						$query2 = pdoQuery("SELECT `ui_geschlecht` FROM `userinfo` WHERE `ui_userid` = :ui_userid", [':ui_userid'=>$row['c_von_user_id']]);
+						$result2Count = $query2->rowCount();
+						if ($result2Count == 1) {
+							$result2 = $query2->fetch();
+							$ui_gen = $result2['ui_geschlecht'];
+						} else {
+							$ui_gen = '0';
+						}
+						$ava = avatar_anzeigen($row['c_von_user_id'], $row['c_von_user'], "chat", $ui_gen);
 					}
-					$ava = avatar_anzeigen($row['c_von_user_id'], $row['c_von_user'], "chat", $ui_gen);
-				}
-				if ($benutzerdaten['u_layout_chat_darstellung'] == '0') {
-					$gesuchter_nutzer = '[zu&nbsp;' . $u_nick . ']';
-					if ( str_contains($row['c_text'], $gesuchter_nutzer) ) {
-						$vonuserid = $ava . " <span class=\"nachrichten_privat\" title=\"". $times ."\"><b>" . $row['c_von_user'] . "</b>: ";
+					if ($benutzerdaten['u_layout_chat_darstellung'] == '0') {
+						$gesuchter_nutzer = '[zu&nbsp;' . $u_nick . ']';
+						if ( str_contains($row['c_text'], $gesuchter_nutzer) ) {
+							$vonuserid = $ava . " <span class=\"nachrichten_privat\" title=\"". $times ."\"><b>" . $row['c_von_user'] . "</b>: ";
+						} else {
+							$vonuserid = $ava . " <span style=\"". $usr_farbe ."\" title=\"". $times ."\"><b>" . $row['c_von_user'] . "</b>: ";
+						}
 					} else {
 						$vonuserid = $ava . " <span style=\"". $usr_farbe ."\" title=\"". $times ."\"><b>" . $row['c_von_user'] . "</b>: ";
 					}
-				} else {
-					$vonuserid = $ava . " <span style=\"". $usr_farbe ."\" title=\"". $times ."\"><b>" . $row['c_von_user'] . "</b>: ";
 				}
-			}
-			
-			// Private Nachrichten
-			if( $row['c_typ'] == 'P' ) {
-				if($benutzerdaten['u_avatare_anzeigen'] == 1) {
-					$query2 = pdoQuery("SELECT `ui_geschlecht` FROM `userinfo` WHERE `ui_userid` = :ui_userid", [':ui_userid'=>$row['c_von_user_id']]);
-					$result2Count = $query2->rowCount();
-					if ($result2Count == 1) {
-						$result2 = $query2->fetch();
-						$ui_gen = $result2['ui_geschlecht'];
-					} else {
-						$ui_gen = '0';
-					}
-					$ava = avatar_anzeigen($row['c_von_user_id'], $row['c_von_user'], "chat", $ui_gen);
-				}
-				if ($benutzerdaten['u_layout_chat_darstellung'] == '0') {
-					$vonuserid = $ava . " <span class=\"nachrichten_privat\" title=\"". $times ."\"><b>". $row['c_von_user'] ."&nbsp;(<a href=\"#\" onMouseOver=\"return(true)\" onClick=\"appendtext_chat('/msg ". $row['c_von_user'] ." '); return(false)\">privat</a>):</b> ";
-				} else {
-					$vonuserid = $ava . " <span style=\"". $usr_farbe ."\" title=\"". $times ."\"><b>". $row['c_von_user'] ."&nbsp;(<a href=\"#\" onMouseOver=\"return(true)\" onClick=\"appendtext_chat('/msg ". $row['c_von_user'] ." '); return(false)\">privat</a>):</b> ";
-				}
-			}
-			
-			// Sprüche und /me
-			if( $row['c_typ'] == 'H' ) {
-				$vonuserid = '';
-				$row['c_text'] = "<i>" . $row['c_text'] . "</i>";
 				
-				if( $row_usr['u_level'] == 'S' || $row_usr['u_level'] == 'C' || $row_usr['u_level'] == 'M' ) {
-					$level = " <b>(". $row['c_von_user'] .")</b>";
+				// Private Nachrichten
+				if( $row['c_typ'] == 'P' ) {
+					if($benutzerdaten['u_avatare_anzeigen'] == 1) {
+						$query2 = pdoQuery("SELECT `ui_geschlecht` FROM `userinfo` WHERE `ui_userid` = :ui_userid", [':ui_userid'=>$row['c_von_user_id']]);
+						$result2Count = $query2->rowCount();
+						if ($result2Count == 1) {
+							$result2 = $query2->fetch();
+							$ui_gen = $result2['ui_geschlecht'];
+						} else {
+							$ui_gen = '0';
+						}
+						$ava = avatar_anzeigen($row['c_von_user_id'], $row['c_von_user'], "chat", $ui_gen);
+					}
+					if ($benutzerdaten['u_layout_chat_darstellung'] == '0') {
+						$vonuserid = $ava . " <span class=\"nachrichten_privat\" title=\"". $times ."\"><b>". $row['c_von_user'] ."&nbsp;(<a href=\"#\" onMouseOver=\"return(true)\" onClick=\"appendtext_chat('/msg ". $row['c_von_user'] ." '); return(false)\">privat</a>):</b> ";
+					} else {
+						$vonuserid = $ava . " <span style=\"". $usr_farbe ."\" title=\"". $times ."\"><b>". $row['c_von_user'] ."&nbsp;(<a href=\"#\" onMouseOver=\"return(true)\" onClick=\"appendtext_chat('/msg ". $row['c_von_user'] ." '); return(false)\">privat</a>):</b> ";
+					}
+				}
+				
+				// Sprüche und /me
+				if( $row['c_typ'] == 'H' ) {
+					$vonuserid = '';
+					$row['c_text'] = "<i>" . $row['c_text'] . "</i>";
+					
+					if( $row_usr['u_level'] == 'S' || $row_usr['u_level'] == 'C' || $row_usr['u_level'] == 'M' ) {
+						$level = " <b>(". $row['c_von_user'] .")</b>";
+					}
+				}
+				
+				// Status-Nachrichten
+				if( $row['c_typ'] == 'S' ) {
+					if ($chat_status_klein) {
+						$row['c_text'] = "<small>" . $row['c_text'] . "</small>";
+					}
+					//$row['c_text'] = "<span class=\"fa-solid fa-circle-info icon16\"></span> <span>" . $row['c_text'];
+				}
+				
+				$c_text = $row['c_text'];
+				
+				// Smilies ausgeben oder unterdrücken
+				if ($benutzerdaten['u_smilies'] == "0") {
+					$c_text = str_replace("<smil ", "<small><SMILIE></small><!--", $c_text);
+					$c_text = str_replace(" smil>", "-->", $c_text);
+				} else {
+					$c_text = str_replace("<smil ", "<img src='images/smilies/style-$benutzerdaten[u_layout_farbe]/", $c_text);
+					$c_text = str_replace(" smil>", "'>", $c_text);
+				}
+				
+				// Leerzeichen Prüfung
+				if(!ctype_space($c_text)) {
+					$line->c_id = $row['c_id'];
+					$line->c_von_user = $row['c_von_user'];
+					$line->c_farbe = $row['c_farbe'];
+					$line->c_text = $c_text;
+					//$line->c_typ = $row['c_typ'];
+					//$line->chat_ausgabe = $ausgabe;
+					$line->level = $level;
+					$line->vonuserid = $vonuserid;
+					$line->c_zeit = date('H:i:s', strtotime($row['c_zeit']));
+					$arr[] = json_encode($line);
 				}
 			}
-			
-			// Status-Nachrichten
-			if( $row['c_typ'] == 'S' ) {
-				if ($chat_status_klein) {
-					$row['c_text'] = "<small>" . $row['c_text'] . "</small>";
-				}
-				//$row['c_text'] = "<span class=\"fa-solid fa-circle-info icon16\"></span> <span>" . $row['c_text'];
-			}
-			
-			$c_text = $row['c_text'];
-			
-			// Smilies ausgeben oder unterdrücken
-			if ($benutzerdaten['u_smilies'] == "0") {
-				$c_text = str_replace("<smil ", "<small><SMILIE></small><!--", $c_text);
-				$c_text = str_replace(" smil>", "-->", $c_text);
-			} else {
-				$c_text = str_replace("<smil ", "<img src='images/smilies/style-$benutzerdaten[u_layout_farbe]/", $c_text);
-				$c_text = str_replace(" smil>", "'>", $c_text);
-			}
-			
-			// Leerzeichen Prüfung
-			if(!ctype_space($c_text))
-			{
-			$line->c_id = $row['c_id'];
-			$line->c_von_user = $row['c_von_user'];
-			$line->c_farbe = $row['c_farbe'];
-			$line->c_text = $c_text;
-			//$line->c_typ = $row['c_typ'];
-			//$line->chat_ausgabe = $ausgabe;
-			$line->level = $level;
-			$line->vonuserid = $vonuserid;
-			$line->c_zeit = date('H:i:s', strtotime($row['c_zeit']));
-			$arr[] = json_encode($line);
-			}
-
 		}
 		
 		// Raum merken
